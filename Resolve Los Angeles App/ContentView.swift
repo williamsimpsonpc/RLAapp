66b5ffb03e7fe3958578668e8ae9969d0cdcf97c
williamsimpsonpc <williamsimpsonpc@gmail.com>
2022-04-06 10:42:04 -0700

    Initial Commit

---
+++ Resolve Los Angeles App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Resolve Los Angeles App
+//
+//  Created by William Simpson on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Resolve Los Angeles App/Resolve_Los_Angeles_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resolve_Los_Angeles_AppApp.swift
+//  Resolve Los Angeles App
+//
+//  Created by William Simpson on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Resolve_Los_Angeles_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
