54eac033eb18422f13a2f251974c0f8ea32e8c17
williamsimpsonpc <williamsimpsonpc@gmail.com>
2022-04-06 15:30:17 -0700

    Test Game

--- Resolve Los Angeles App/ContentView.swift
+++ Resolve Los Angeles App/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Git Hub Test!")
-            .padding()
+        
+        
+        ZStack{
+            
+        Image("background")
+                .ignoresSafeArea()
+        VStack{
+            
+            Spacer()
+            Image("logo")
+            Spacer()
+            
+                HStack{
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+            
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                    
+                HStack{
+                    Spacer()
+                    Text("Player")
+                        .foregroundColor(.white)
+                    Spacer()
+                    Text("CPU")
+                        .foregroundColor(.white)
+                    Spacer()
+                }
+            
+                HStack{
+                Spacer()
+                Text("0")
+                    .foregroundColor(.white)
+                Spacer()
+                Text("0")
+                    .foregroundColor(.white)
+                Spacer()
+            }
+                
+            Group {
+            Spacer()
+            Spacer()
+            }
+        }
     }
+}
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
