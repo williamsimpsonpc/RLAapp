28bc0d81817590ef4ac280fef64f459666673eae
williamsimpsonpc <williamsimpsonpc@gmail.com>
2022-04-12 11:18:15 -0700

    Buttons and reactive now

--- Resolve Los Angeles App/ContentView.swift
+++ Resolve Los Angeles App/ContentView.swift
@@ -8,59 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+
+    
     var body: some View {
-        
         
         ZStack{
             
         Image("background")
                 .ignoresSafeArea()
-        VStack{
             
+        VStack {
             Spacer()
             Image("logo")
             Spacer()
-            
-                HStack{
-                    Spacer()
-                    Image("card3")
-                    Spacer()
-                    Image("card4")
-                    Spacer()
-                }
-            
+            HStack{
                 Spacer()
-                Image("dealbutton")
+                Image(playerCard)
                 Spacer()
-                    
-                HStack{
-                    Spacer()
-                    Text("Player")
-                        .foregroundColor(.white)
-                    Spacer()
-                    Text("CPU")
-                        .foregroundColor(.white)
-                    Spacer()
-                }
-            
-                HStack{
-                Spacer()
-                Text("0")
-                    .foregroundColor(.white)
-                Spacer()
-                Text("0")
-                    .foregroundColor(.white)
+                Image(cpuCard)
                 Spacer()
             }
                 
-            Group {
-            Spacer()
-            Spacer()
+                Spacer()
+                
+                Button(action: {
+                    //Generate a random number between 2...14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    //Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    //Update the score
+                    //playerScore += 1
+                    //cpuScore += 1
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                }
+                Spacer()
             }
+            Spacer()
+        }
         }
     }
-}
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
