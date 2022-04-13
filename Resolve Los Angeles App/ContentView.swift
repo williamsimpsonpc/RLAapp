1cbfb6870403129abf8084e1b6c4d4bcd82854f2
williamsimpsonpc <williamsimpsonpc@gmail.com>
2022-04-12 21:40:17 -0700

    Home Screen Addition

--- Resolve Los Angeles App/ContentView.swift
+++ Resolve Los Angeles App/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+//------------BODY
 struct ContentView: View {
     
     @State private var playerCard = "card5"
@@ -18,75 +19,112 @@ struct ContentView: View {
     var body: some View {
         
         ZStack{
-            Color("background")
-                .edgesIgnoringSafeArea(.all)
             
-        VStack {
-            Spacer()
-            Image("logo")
-            Spacer()
-            HStack{
-                Spacer()
-                Image(playerCard)
-                Spacer()
-                Image(cpuCard)
-                Spacer()
-            }
-                
-                Spacer()
-                
-                Button(action: {
-                    //Generate a random number between 2...14
-                    let playerRand = Int.random(in: 2...14)
-                    let cpuRand = Int.random(in: 2...14)
+        Color("background")
+            .edgesIgnoringSafeArea(.all)
+            
+                VStack {
+                    GifImage("spinslow")
+                        .frame(width: 175, height: 175)
                     
-                    //Update the cards
-                    playerCard = "card" + String(playerRand)
-                    cpuCard = "card" + String(cpuRand)
-                    
-                    //Update the score
-                    if playerRand > cpuRand {
-                    playerScore += 1
-                    }else if cpuRand > playerRand{
-                    cpuScore += 1
+                    Group{
+                        Spacer()
                     }
                     
-                }, label: {
-                    Image("dealbutton")
-                })
-                Spacer()
-                HStack{
-                    Spacer()
-                    VStack {
-                        Text("Player")
-                            .font(.headline)
+                
+//----------SHOP
+                    
+                    Button(action: {
+                        //Generate a random number between 2...14
+                        let playerRand = Int.random(in: 2...14)
+                        let cpuRand = Int.random(in: 2...14)
+                        
+                        //Update the cards
+                        playerCard = "card" + String(playerRand)
+                        cpuCard = "card" + String(cpuRand)
+                        
+                        //Update the score
+                        if playerRand > cpuRand {
+                        playerScore += 1
+                        }else if cpuRand > playerRand{
+                        cpuScore += 1
+                        }
+                        
+                    }, label: {
+                        Text("SHOP")
                             .foregroundColor(Color.white)
-                            .padding(.bottom, 10.0)
-                        Text(String(playerScore))
-                            .font(.largeTitle)
-                            .foregroundColor(Color.white)
+                    })
+                    
+                    Group{
+                        Spacer()
                     }
-                    Spacer()
-                    VStack{
-                        Text("CPU")
-                            .font(.headline)
+                    
+//-----------MISSION
+                    Button(action: {
+                        //Generate a random number between 2...14
+                        let playerRand = Int.random(in: 2...14)
+                        let cpuRand = Int.random(in: 2...14)
+                        
+                        //Update the cards
+                        playerCard = "card" + String(playerRand)
+                        cpuCard = "card" + String(cpuRand)
+                        
+                        //Update the score
+                        if playerRand > cpuRand {
+                        playerScore += 1
+                        }else if cpuRand > playerRand{
+                        cpuScore += 1
+                        }
+                        
+                    }, label: {
+                        Text("MISSION")
                             .foregroundColor(Color.white)
-                            .padding(.bottom, 10.0)
-                        Text(String(cpuScore))
-                            .font(.largeTitle)
+                    })
+                    
+                    
+                    Group{
+                        Spacer()
+                    }
+                    
+                    
+//-----------OUR STORY
+                    Button(action: {
+                        //Generate a random number between 2...14
+                        let playerRand = Int.random(in: 2...14)
+                        let cpuRand = Int.random(in: 2...14)
+                        
+                        //Update the cards
+                        playerCard = "card" + String(playerRand)
+                        cpuCard = "card" + String(cpuRand)
+                        
+                        //Update the score
+                        if playerRand > cpuRand {
+                        playerScore += 1
+                        }else if cpuRand > playerRand{
+                        cpuScore += 1
+                        }
+                        
+                    }, label: {
+                        Text("OUR STORY")
                             .foregroundColor(Color.white)
-                }
-                Spacer()
-            }
+                    })
+                    
+                    
+                Group{
             Spacer()
+            Spacer()
+            }
+                    
         }
         }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
             
     }
 }
