b5a71a0471dbb696270821f36561b15e6c631c81
williamsimpsonpc <williamsimpsonpc@gmail.com>
2022-04-13 14:58:37 -0700

    Mission new name

---
+++ Resolve Los Angeles App/MissionView.swift
@@ -0,0 +1,20 @@
+//
+//  missionView.swift
+//  Resolve Los Angeles App
+//
+//  Created by William Simpson on 4/13/22.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text("Mission Page")
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionView()
+    }
+}
